e96c49e5ee3c597a151f4804b532801d31dc5040
Diogo Martino <diogo.martino10@gmail.com>
2026-03-25 21:06:46 +0000

    chore: add full screen template

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,9 +3,11 @@ import {
   type TPluginComponentsMapBySlotId,
 } from "@sharkord/plugin-sdk";
 import { Home } from "./components/home";
+import { FullScreen } from "./components/full-screen";
 
 const components: TPluginComponentsMapBySlotId = {
   [PluginSlot.HOME_SCREEN]: [Home],
+  [PluginSlot.FULL_SCREEN]: [FullScreen],
 };
 
 export { components };
