b7f8f7c388faf2f82e7c44d2912444a94578ca43
Diogo Martino <diogo.martino10@gmail.com>
2026-03-25 21:25:22 +0000

    chore: update publish script

--- publish.ts
+++ publish.ts
@@ -3,7 +3,7 @@ import { PLUGIN_SDK_VERSION } from "@sharkord/plugin-sdk";
 
 const result = await publish({
   githubToken: process.env.GITHUB_TOKEN,
-  repo: "Sharkord/music-bot",
+  repo: "owner/repo",
   sdkVersion: PLUGIN_SDK_VERSION,
 });
 
